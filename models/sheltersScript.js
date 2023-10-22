0c8b4708fa83177a47a51fd73359a23c97e085e3
aSeaOfCringe <rutaraudzina@gmail.com>
2023-10-22 17:27:07 +0300

    Shelters list map in progress

--- models/sheltersScript.js
+++ models/sheltersScript.js
@@ -8,7 +8,23 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
+
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
         console.log(position);
+
+        const coords = [latitude, longitude];
+
+        var map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+            .openPopup();
     }, function () {
         alert(`Nevar noteikt atrašanās vietu`);
     });
--- views/shelters.php
+++ views/shelters.php
@@ -5,25 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shelters list</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script defer src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
     <link href="css\sheltersStyles.css" rel="stylesheet" />
 
-    <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="shortcut icon" type="image/png" href="/icon.png" /> -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="shortcut icon" type="image/png" href="/icon.png" />
 
     <!-- <link
       href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;700;800&display=swap"
       rel="stylesheet"
-    />
-
-    <link rel="stylesheet" href="style.css" /> -->
+    /> -->
 
     <script defer src="models/sheltersScript.js"></script>
+
 </head>
 <body>
 
+<div id="wrapper">
     <div class="sidebar">
-      <!-- <img src="logo.png" alt="Logo" class="logo" /> -->
-
       <ul class="shelters">
         <li class="workout workout--running" data-id="1234567890">
 
@@ -55,34 +57,11 @@
 
         </li>
 
-        <!-- <li class="workout workout--running" data-id="1234567890">
-          <h2 class="workout__title">Running on April 14</h2>
-          <div class="workout__details">
-            <span class="workout__icon">🏃‍♂️</span>
-            <span class="workout__value">5.2</span>
-            <span class="workout__unit">km</span>
-          </div>
-          <div class="workout__details">
-            <span class="workout__icon">⏱</span>
-            <span class="workout__value">24</span>
-            <span class="workout__unit">min</span>
-          </div>
-          <div class="workout__details">
-            <span class="workout__icon">⚡️</span>
-            <span class="workout__value">4.6</span>
-            <span class="workout__unit">min/km</span>
-          </div>
-          <div class="workout__details">
-            <span class="workout__icon">🦶🏼</span>
-            <span class="workout__value">178</span>
-            <span class="workout__unit">spm</span>
-          </div>
-        </li> -->
-
       </ul>
 
     </div>
 
     <div id="map"></div>
+</div>
 </body>
 </html>
