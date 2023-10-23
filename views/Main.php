00ab39576a52cacf4d7bb57ea8e35d441a247f6f
Boginja228 <jmacushy@gmail.com>
2023-10-23 15:59:46 +0300

    Main+Header

--- views/Main.php
+++ views/Main.php
@@ -1,9 +1,7 @@
-DESKTOP -6
-
-
+<!-- DESKTOP -6 -->
 <!-- TODO DELETE THIS IN FUTURE -->
 <!-- podzinas lai pariet uz citam lapam -->
-<div style="border: 1px solid black; width:150px;margin: 10px;margin-top: 100px">
+<!-- <div style="border: 1px solid black; width:150px;margin: 10px;margin-top: 100px">
     <a href="news">Jaunimi</a><br>
     <a href="advertismentAdd">Jauns sludinajums</a><br>
     <a href="advertisment">Sludinajums</a><br>
@@ -18,4 +16,23 @@ DESKTOP -6
     <a href="shelter">Patversme</a><br>
     <a href="shelters">Patversmes</a><br>
     <a href="shops">Veikali</a><br>
+</div> -->
+<link href="css\stylesMain.css" rel="stylesheet" />
+<div>        
+    <div class="images-paw">
+            <a href="news"><img src="paw.png" alt="Adoption"></a>
+    </div>
+    <div class="images-search">
+            <a href="news"><img src="search.png" alt="Lost pet"></a>
+    </div>
+    <div class="images-shelter">
+            <a href="shelters"><img src="shelter.png" alt="Shelters"></a>
+    </div>
+   
+    <p class="adopt" >Adoptēt</p>
+
+    <p class="lost" >Pazaudēts dzīvnieks</p>
+
+    <p class="shelter">Patversmju saraksts</p>
+
 </div>
