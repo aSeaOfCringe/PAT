42ce6817fe84272ea66857701b1e816a33e48542
prolgan <prolgan@inbox.lv>
2023-10-27 21:45:49 +0300

    DBFileFix

--- controls/DB.php
+++ controls/DB.php
@@ -1,4 +1,6 @@
 <?php
+
+//git update-index --assume-unchanged controls/DB.php 
 Class DBase{
         private $db_ip = "";
         private $login = "";
@@ -10,13 +12,6 @@ Class DBase{
         function conncect(){
             mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
             $this->connection = mysqli_connect($this->db_ip,$this->login,$this->psw,$this->db_name) or die(mysqli_error($this->connection));
-        }
-
-        function test(){
-            $testQuery = mysqli_query($this->connection,
-            "SELECT * FROM `testTable`");
-		    $testString = mysqli_fetch_assoc($testQuery);
-            echo($testString["tekst"]);
         }
 
         function __construct(){
