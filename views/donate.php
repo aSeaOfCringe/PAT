7bc88b36b549ba0f50524861e69997f925ec6a71
Boginja228 <jmacushy@gmail.com>
2023-10-27 10:45:22 +0300

    page merge & donate

--- views/donate.php
+++ views/donate.php
@@ -1,1 +1,19 @@
-Desktop-12
+<!-- Desktop-12 -->
+<link href="css\stylesDonate.css" rel="stylesheet" />
+<div>
+    <p class="text">Ziedo dzīvniekiem</p>
+    <p class="text right">Mūsu rekvizīti</p>
+
+    <div class="custom-select">
+        <input type="text" id="textBox" placeholder="Ziedojuma projekts, ko vēlies atbalstīt">
+            <ul class="options">
+                <li>Projekts1</li>
+                <li>Projekts2</li>
+                <li>Projekts3</li>
+            </ul>
+    </div>
+
+
+
+
+</div>
