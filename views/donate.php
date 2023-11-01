98a1d2fd307f18b7add8d2c8e005346984d74fec
aSeaOfCringe <rutaraudzina@gmail.com>
2023-11-01 22:07:20 +0200

    Veronikas kods

--- views/donate.php
+++ views/donate.php
@@ -4,16 +4,26 @@
     <p class="text">Ziedo dzīvniekiem</p>
     <p class="text right">Mūsu rekvizīti</p>
 
-    <div class="custom-select">
-        <input type="text" id="textBox" placeholder="Ziedojuma projekts, ko vēlies atbalstīt">
-            <ul class="options">
-                <li>Projekts1</li>
-                <li>Projekts2</li>
-                <li>Projekts3</li>
-            </ul>
+    <div>
+        <select name="option" class="custom-select">
+            <option value="" disabled selected>Ziedojuma projekts, ko vēlies atbalstīt</option>
+            <option value="1">Projekts1</option>
+            <option value="2">Projekts2</option>
+            <option value="3">Projekts3</option>
+        </select>
+        <input type="text" id="textBox1" placeholder="Ziedotāja vārds">
+        <input type="text" id="textBox2" placeholder="Ziedojuma apraskts">
+        <input type="text" id="textBox3" placeholder="Summa">
+        <button class="btn-donate">Ziedo</button>
     </div>
 
-
+    <div>
+        <p class="black">Reģistrācijas Nr.XXXXXXXXXXXX</p>
+        <P class="black next1">Banks: A/S SWEDBANK</P>
+        <p class="black next2">Kods: HABALV22</p>
+        <p class="black next3">Konts:LVXXXXXXXXXXXX</p>
+        <P class="black next4">Paldies par atbalstu!</P>
+    </div>
 
 
 </div>
